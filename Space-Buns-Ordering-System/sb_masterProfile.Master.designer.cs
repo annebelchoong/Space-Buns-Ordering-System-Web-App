5752085989cba48ff44e462da01118d5135fdd49
chiahuiyun <80665061+chiahuiyun@users.noreply.github.com>
2022-08-24 22:30:46 +0800

    edit profile design

--- Space-Buns-Ordering-System/sb_masterProfile.Master.designer.cs
+++ Space-Buns-Ordering-System/sb_masterProfile.Master.designer.cs
@@ -87,6 +87,15 @@ namespace Space_Buns_Ordering_System
         protected global::System.Web.UI.WebControls.HyperLink hlNavOrder;
 
         /// <summary>
+        /// btnEditPic control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEditPic;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
