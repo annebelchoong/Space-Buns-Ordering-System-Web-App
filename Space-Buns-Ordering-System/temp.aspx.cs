f4e8ed5483889882395a30f3860d8edb51af422d
Yong Yang <55826849+boonyongyang@users.noreply.github.com>
2022-09-13 00:45:07 +0800

    Temp file for branch

--- Space-Buns-Ordering-System/temp.aspx.cs
+++ Space-Buns-Ordering-System/temp.aspx.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data.SqlClient;
-using System.Configuration;
 
 namespace Space_Buns_Ordering_System
 {
-    public partial class adminBranches : System.Web.UI.Page
+    public partial class temp : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvOrderList_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
